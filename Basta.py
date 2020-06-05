169ad415debdbec89a72e9cf4fcb37f1f52d14c9
EAFD88 <edd.danon@gmail.com>
2020-06-05 16:01:02 +0100

    Added Franchise class, with init and repr

--- Basta.py
+++ Basta.py
@@ -1,5 +1,7 @@
 # Codecademy Basta Fazoolin' project
+
 class Menu:
+
 	def __init__(self, name, items, start_time, end_time):
 		self.name=name
 		self.items=items
@@ -16,11 +18,24 @@ class Menu:
 		return total
 
 
+class Franchise:
+
+	def __init__(self, address, menus):
+		self.address = address
+		self.menus = menus
+
+	def __repr__(self):
+		print("This branch is at {address}".format(address=self.address))
+
+
 brunch = Menu("Brunch",{'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50},11,16)
 early_bird = Menu("Early-bird", {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00},15,18)
 dinner = Menu("Dinner",{'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,},17,23)
 kids = Menu("Kids",{'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00},11,21)
 
-print(brunch)
+#print(brunch)
+#print(brunch.calculate_bill(["pancakes","home fries","coffee"]))
+#print(early_bird.calculate_bill(["salumeria plate","mushroom ravioli (vegan)"]))
 
-print(brunch.calculate_bill(["pancakes","home fries","coffee"]))
+flagship_store = Franchise("1232 West End Road", [brunch, early_bird, kids, dinner])
+new_installment = Franchise("12 East Mulberry Street", [brunch, early_bird, kids, dinner])
